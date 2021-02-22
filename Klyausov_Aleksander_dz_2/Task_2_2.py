929107b29ff9676468cf3773718df2878c71492e
AlexKlaus <krounix@gmail.com>
2021-02-22 19:47:27 +0300

    Обработал список, обернул числа кавычками и дополнил нулем до двух разрядов

---
+++ Klyausov_Aleksander_dz_2/Task_2_2.py
@@ -0,0 +1,25 @@
+weather = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+print("Id:", id(weather))
+
+for item in weather:
+
+    # Если строка содержит знаки '+' или '-', и оставшаяса чать является числом, и длина числа меньше 2
+    if ('+' in item or '-' in item) and item[1:].isdigit() and len(item[1:]) < 2:
+        weather.insert((weather.index(item)), '"')  # добавлем кавычки
+        weather.insert((weather.index(item)+1), '"')
+        weather[weather.index(item)] = f"{item[0]}0{item[1:]}"  # добавляем нуль до двух разрядов
+
+    # Если строка является числом и длина меньше 2, то добавляем разрядность и кавычки
+    elif item.isdigit() and len(item) < 2:
+        weather.insert((weather.index(item)), '"')  # добавлем кавычки
+        weather.insert((weather.index(item) + 1), '"')
+        weather[weather.index(item)] = f"0{item}"  # добавляем нуль до двух разрядов
+
+    # Если строка является числом, и перед нет кавычек, то ставим кавычки
+    # Если не делать проверку на кавычки, то после первой итерации происходит зацикливание
+    # т.к. после добавления кавычек число смещается.
+    elif item.isdigit() and (weather[weather.index(item) - 1] != '"'):
+        weather.insert((weather.index(item)), '"')  # добавлем кавычки
+        weather.insert((weather.index(item) + 1), '"')
+print(id(weather))
+print(weather)
