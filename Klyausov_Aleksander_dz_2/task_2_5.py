65ae95291d714663c6e13b89fbfe5c536a18e3f9
AlexKlaus <krounix@gmail.com>
2021-02-23 15:31:10 +0300

    refactor task5

--- Klyausov_Aleksander_dz_2/task_2_5.py
+++ Klyausov_Aleksander_dz_2/task_2_5.py
@@ -1,5 +1,5 @@
 prices = [57.8, 46.51, 97, 120, 185, 18284, 2519, 480, 333.3, 787.44, 30.01, 54.7, 65.89, 488.1, 666.6]
-reverce_price = []
+reverse_price = []
 print("Id в начале:", id(prices))
 prices_str = ''
 n = 0   # счетчик
@@ -16,10 +16,10 @@ for p in sorted(prices):
         prices_str += f'{price[0]} руб {price[1]} коп'  # заполняем строку в нужном формате
     else:
         prices_str += f'{price[0]} руб {price[1]} коп, '  # заполняем строку в нужном формате
-    reverce_price.insert(0, f'{price[0]} руб {price[1]} коп')  # сразу создаем реверсирвованый список в нужном формате
+    reverse_price.insert(0, f'{price[0]} руб {price[1]} коп')  # сразу создаем реверсирвованый список в нужном формате
     n += 1
 
 print("Цены по возрастанию: ", prices_str)
-print("Цены по убыванию: ", ', '.join(reverce_price))
-print("Пять самых дорогих товаров: ", ', '.join(reverce_price[:5]))
+print("Цены по убыванию: ", ', '.join(reverse_price))
+print("Пять самых дорогих товаров: ", ', '.join(reverse_price[:5]))
 print("Id в конце", id(prices))
